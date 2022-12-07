856ba79594be35dd1a423631c3283ebb46e9f65c
Sultan <sultan@yahoo.com>
2022-12-07 17:59:14 +0100

    CRUD created gor user

--- src/Form/User1Type.php
+++ src/Form/User1Type.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class User1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
